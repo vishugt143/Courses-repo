4f148634dd1215c185c211177ac530c62d1804f5
Amanraj654 <paymentaman6+github@gmail.com>
2023-04-23 08:19:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6033128760:AAHOPlmcQHxBxabnhRJtcGWAx_xC-l_SKfc"
+    BOT_TOKEN = "6188508867:AAHq0aDD1ohKXRjB5SNzVipMch14C9_QCSw"
     API_ID = 23365392
     API_HASH = "fffdba007ae9e02207be11b8d8d3f2a8"
     AUTH_USERS = '5757528547'
