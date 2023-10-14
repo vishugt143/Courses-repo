c3d63e27800c4bc5ffc0afa5fc0b7761c0910b2f
pjvip <140431425+pjvip@users.noreply.github.com>
2023-10-14 12:19:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", "10551685"))
-    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = "6400468647:AAHd3WGvsIQWg47kBijOuxE1aM_8H1WUuFI"
+    API_ID = "10551685"
+    API_HASH = "477dff815fdc7cba803c1bc02c459d03"
     AUTH_USERS = '1112773045,1975696269'
 
